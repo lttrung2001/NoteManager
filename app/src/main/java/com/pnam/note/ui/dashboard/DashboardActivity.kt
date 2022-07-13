75e33345073b9258bbc171910f7b638c55b8202e
Le Thanh Trung <lt.trung2001@gmail.com>
2022-07-13 16:53:42 +0700

    init

---
+++ app/src/main/java/com/pnam/note/ui/dashboard/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package com.pnam.note.ui.dashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pnam.note.R
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
