c5bbfad8cc79b8392767d7aaec267a7ce529ea48
Le Thanh Trung <lt.trung2001@gmail.com>
2023-01-22 10:56:18 +0700

    using ip

--- app/src/main/java/com/pnam/note/utils/RetrofitUtils.kt
+++ app/src/main/java/com/pnam/note/utils/RetrofitUtils.kt
@@ -1,10 +1,9 @@
 package com.pnam.note.utils
 
 object RetrofitUtils {
-//    private const val PORT: Int = 3000
-//    private const val STATIC_IP: String = "192.168.1.7"
-//    private const val IP_URL: String = "$STATIC_IP:$PORT"
-//    const val BASE_URL: String = "http://$IP_URL/"
-    const val BASE_URL: String = "https://note-api-production-79b5.up.railway.app/"
+    private const val PORT: Int = 3000
+    private const val STATIC_IP: String = "192.168.39.118"
+    private const val IP_URL: String = "$STATIC_IP:$PORT"
+    const val BASE_URL: String = "http://$IP_URL/"
     const val SUCCESS: Int = 200
 }
